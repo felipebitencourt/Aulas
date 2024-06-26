00f33cb53499fed0608ad8a34906b4ca3d4e61c6
Felipe Bitencourt <felipebmspt@gmail.com>
2024-06-26 21:15:26 +0100

    Aula 56

---
+++ 04_python/aula_057/exercicio_01/main.py
@@ -0,0 +1,8 @@
+print("\n\n")
+
+print("=== Dados ===")
+print("Nome: Felipe Bitencourt")
+print("Morada: Póvoa de Varzim")
+print("Iade: 27")    
+
+print("\n\n")
---
+++ 04_python/aula_057/exercicio_02/main.py
@@ -0,0 +1,11 @@
+print("\n\n")
+
+exemplo_string = "Felipe Bitencourt" #string = qq texto
+exemlo_int = 5                       #int = números inteiros
+exemlo_float = 5.55                  #float = números q tem decimais
+
+print(exemplo_string)
+print(exemlo_int)
+print(exemlo_float)
+
+print("\n\n")
